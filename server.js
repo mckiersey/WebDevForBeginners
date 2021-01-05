b36ba242737200de3f583e88573661ef719dbc69
Sean McKiernan <seanmckiernan01@gmail.com>
2021-01-05 22:14:35 +0000

    home page and server

---
+++ server.js
@@ -0,0 +1,16 @@
+// We need to create a bunch of variables to set up the server
+// const is (one of the ways) to create variables in javascript
+
+const express = require('express');
+const app = express();
+const port = 80;
+const path = require('path');
+
+
+//start the server
+const server = app.listen(port, (error) => {
+    if(error) return console.log(`** SERVER ERROR: ${error}`);
+
+    console.log(`Server is running on port: ${server.address().port}`);
+
+});
