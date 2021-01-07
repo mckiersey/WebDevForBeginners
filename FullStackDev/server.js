e3a3ae871438cf4eb17797cc4ac98b2647bffee9
Sean McKiernan <seanmckiernan01@gmail.com>
2021-01-07 19:14:31 +0000

    adding route to server

--- FullStackDev/server.js
+++ FullStackDev/server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 const port = 80;
 const path = require('path');
-const routes = require('/routes');
+const routes = require('./routes.js');
 
 
 
