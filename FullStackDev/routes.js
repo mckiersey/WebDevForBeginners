a6506eac5d48324b1377ecd19bcaf4fbd6339cf8
Sean McKiernan <seanmckiernan01@gmail.com>
2021-01-10 18:25:12 +0000

    homepage logic to post new user data to back end

--- FullStackDev/routes.js
+++ FullStackDev/routes.js
@@ -1,3 +1,6 @@
+// LINK TO DATABASE CONNECTION
+const pool = require('./config.js')
+
 // DEFINE APP
 const router = app => {
 
@@ -9,6 +12,11 @@ const router = app => {
     app.get('/home', (request, response)=>{
         response.sendFile("/Users/Seansmac/Desktop/Dev/Full_stack_for_absolute_beginners/myrepo/FullStackDev/homepage.html")
     });
+
+    pool.query('SELECT * FROM user_profile;', function (err, result, fields) {
+        if (err) throw new Error(err)
+        // Do something with result.
+    })
 
 
 
