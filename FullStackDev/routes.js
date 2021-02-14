2b0a6340ac95e39ece2fc1ff3ef9cefe6cc80ca3
Sean McKiernan <seanmckiernan01@gmail.com>
2021-02-14 20:55:38 +0000

    after Kristo call it seems like the bugs have been ironed out

--- FullStackDev/routes.js
+++ FullStackDev/routes.js
@@ -20,26 +20,26 @@ async function verify(CLIENT_ID, token) {
             idToken: token,
             audience: CLIENT_ID
         });
+        console.log('post verification')
+
+        const payload = ticket.getPayload();
+        console.log(payload)
+        const SubmittedUserId = payload['sub'];
+        const UserName = payload.name
+        return SubmittedUserId
+
     } catch (error) {
         console.log('im in verify false')
         return false
     }
-    const payload = ticket.getPayload();
-    console.log(payload)
-    const SubmittedUserId = payload['sub'];
-    return SubmittedUserId
+
+
 };
 
 
 
-/*    
-}); 
-    verify().
-    response.cookie('MYSESSIONTOKEN', token) // passing a verified token to the browser
-    response.send('success');
-    console.log('token set')
-    catch('error caught:', console.error); // end of verify async function
-*/
+
+
 
 // DEFINE APP
 const router = app => {
@@ -67,25 +67,27 @@ const router = app => {
 
     // An API CALL THAT REQUIRES VERIFICATION FIRST
     app.post('/userA', async (request, response) => {
-        console.log(request.body.token)
         token = request.body.token
 
-
+        console.log('token = ', token)
+        user = await verify(CLIENT_ID, token)
+        console.log('value of verify function: ', user)
         if (!user) { //if verify function returns false (not user)
             console.log('User not logged in')
+            response.send('User not logged in')
+
             // add login redirect           
+        } else {
+            // next logic
+            console.log('verified user: ', user)
+            // example: protected query
+            pool.query(`SELECT * FROM auth_data`, (error, result) => {
+                if (error) throw error;
+
+                response.send(result);
+                console.log('all users: ' + result);
+            });
         }
-        // next logic
-
-
-
-        console.log('verified user: ', user)
-        pool.query(`SELECT * FROM auth_data`, (error, result) => {
-            if (error) throw error;
-
-            response.send(result);
-            console.log('all users: ' + result);
-        });
 
     });
 
@@ -100,38 +102,47 @@ const router = app => {
 
     // POST NEW USER TO DATABASE
     app.post('/login', async (request, response) => {
+        console.log('login api call: ', request.body)
+
         let token = request.body.token
+        response.cookie('MYSESSIONTOKEN', token) // passing a verified token to the browser
+        response.send('Token set');
+        console.log('token set')
+
         //var NewuserName = request.body.userName
         //var NewuserEmail = request.body.userEmail
 
-
-
-        var NewUserDetails = { user_id: SubmittedUserId, email: NewuserEmail }
-        // check if user already exists in database
-        try {
-            pool.query("SELECT user_id FROM auth_data WHERE user_id = ?", SubmittedUserId, function (error, result, field) {
-                if (error) throw error;
-                console.log('error type:', error);
-
-                if (result.length === 0) {
-                    console.log('inserting new user')
-                    //new user logic
-                    pool.query('INSERT INTO auth_data SET ?', NewUserDetails, (error, result) => {
-                        response.send('New User ' + SubmittedUserId + ' Written to database')
-                        if (error) throw error;
-                        console.log('error type:', error);
-                    });
-                } else {
-                    response.send("Existing user- signing in");
-                }
-            });
-
-        } catch (err) {
-            console.log('backend fail: ' + err)
+    });
+    /*
+            var NewUserDetails = { user_id: SubmittedUserId, email: NewuserEmail }
+            // check if user already exists in database
+            try {
+                pool.query("SELECT user_id FROM auth_data WHERE user_id = ?", SubmittedUserId, function (error, result, field) {
+                    if (error) throw error;
+                    console.log('error type:', error);
+    
+                    if (result.length === 0) {
+                        console.log('inserting new user')
+                        //new user logic
+                        pool.query('INSERT INTO auth_data SET ?', NewUserDetails, (error, result) => {
+                            response.send('New User ' + SubmittedUserId + ' Written to database')
+                            if (error) throw error;
+                            console.log('error type:', error);
+                        });
+                    } else {
+                        response.send("Existing user- signing in");
+                    }
+                });
+    
+            } catch (err) {
+                console.log('backend fail: ' + err)
+            }
+            
         }
-    }
-
-    )
+        
+    
+        )
+        */
 
 };
 module.exports = router;
