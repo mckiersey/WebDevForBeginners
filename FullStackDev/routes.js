ff9bb6c95597e39b6f457c105d18466385c540c3
Sean McKiernan <seanmckiernan01@gmail.com>
2021-03-02 09:19:43 +0000

    final versions of homepage and routes file

--- FullStackDev/routes.js
+++ FullStackDev/routes.js
@@ -4,12 +4,11 @@ var path = require("path");
 
 // google auth
 const { OAuth2Client } = require('google-auth-library');
-const CLIENT_ID = '170958026096-1delfs3g8tg4hoeg6bgs5ickhpe7k5pt.apps.googleusercontent.com'
-const client = new OAuth2Client(CLIENT_ID);
+const CLIENT_ID = '170958026096-1delfs3g8tg4hoeg6bgs5ickhpe7k5pt.apps.googleusercontent.com' // Given by Google when you set up your Google OAuth client ID: https://developers.google.com/identity/sign-in/web/sign-in
 
-//VERIFICATION FUNCTION
+//VERIFICATION FUNCTION: The ticket inspector
 
-// Check authenticity of user id
+// Check authenticity of user id - basically like ticket inspector
 // take the token and verify it and if it is good then gives back user id (from google)
 // if token is 'wrong' (expired/ doesn't exist etc.) it returns false
 // so this only returns user if if an active true token exists in the browser
@@ -17,13 +16,13 @@ const client = new OAuth2Client(CLIENT_ID);
 async function verify(CLIENT_ID, token) {
     const client = new OAuth2Client(CLIENT_ID);
     try {
-        const ticket = await client.verifyIdToken({
+        const ticket = await client.verifyIdToken({ // Function to inspect the user's "ticket" (token)
             idToken: token,
             audience: CLIENT_ID
         });
         console.log('User Verified')
 
-        const payload = ticket.getPayload();
+        const payload = ticket.getPayload(); // The verified token gives back a ticket. This ticket contains things like user ID, email and profile picture (all from a user's Google Account)
         console.log(payload)
         const AuthUserId = payload.sub;
         const UserName = payload.name;
@@ -32,7 +31,7 @@ async function verify(CLIENT_ID, token) {
         return [AuthUserId, UserName, UserEmail, UserPicture]
 
     } catch (error) {
-        console.log('im in verify false')
+        console.log('Token not verified')
         return false
     }
 
@@ -40,23 +39,9 @@ async function verify(CLIENT_ID, token) {
 };
 
 
-
-
-
-
 // DEFINE APP
 const router = app => {
 
-
-    app.get('/', (request, response) => {
-        response.send("So this message is coming from your app, which is also known as your server!");
-    });
-
-
-    app.get('/home', (request, response) => {
-        homepage_file = "/Users/Seansmac/Desktop/Dev/Full_stack_for_absolute_beginners/myrepo/FullStackDev/homepage.html"
-        response.sendFile(homepage_file)
-    });
 
     // POST NEW USER TO DATABASE
     app.post('/SignIn', async (request, response) => {
@@ -112,18 +97,6 @@ const router = app => {
         }
     });
 
-
-
-    app.get('/users', (request, response) => {
-        pool.query(`SELECT * FROM auth_data`, (error, result) => {
-            if (error) throw error;
-
-            response.send(result);
-            console.log('all users: ' + result);
-        });
-
-    });
-
     // An API CALL THAT REQUIRES VERIFICATION FIRST
     app.post('/ProtectedRoute', async (request, response) => {
         token = request.body.token
@@ -137,17 +110,34 @@ const router = app => {
         }
     });
 
+    // Please refer to the schematic to understand how /ProtectedRoute is related to /ProtectedProfile
     app.get('/ProtectedProfile', (request, response) => {
         console.log('in protected profile get request')
         response.render("ProtectedProfile.ejs", { message: "Private profile message" });
     })
 
 
-    // https://stackoverflow.com/a/65006287/6065710 - getting tokens
+    app.get('/home', (request, response) => {
+        homepage_file = "/Users/Seansmac/Desktop/Dev/Full_stack_for_absolute_beginners/myrepo/FullStackDev/homepage.html"
+        response.sendFile(homepage_file)
+    });
 
+    // This is optional, but it is a handy way to see the list of users who have been signed up to your app.
+    // Just type http://localhost/users to trigger this and it'll show the list of users signed up.
+    app.get('/users', (request, response) => {
+        pool.query(`SELECT * FROM auth_data`, (error, result) => {
+            if (error) throw error;
+
+            response.send(result);
+            console.log('all users: ' + result);
+        });
+
+    });
+
+    // Simple sign out route 
     app.get('/SignOut', (req, res) => {
         console.log('sign out route')
-        res.clearCookie('USER_SESSION_TOKEN');
+        res.clearCookie('USER_SESSION_TOKEN'); // This works by clearing the cookies from a user's browsers. No cookie = no token.
         res.redirect('/LoggedOutPage')
 
     })
