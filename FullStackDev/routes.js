541506de72475c03d19fa6f07a5bb920a5c6fe9c
Sean McKiernan <seanmckiernan01@gmail.com>
2021-03-03 12:37:05 +0000

    restart computer

--- FullStackDev/routes.js
+++ FullStackDev/routes.js
@@ -23,7 +23,6 @@ async function verify(CLIENT_ID, token) {
         console.log('User Verified')
 
         const payload = ticket.getPayload(); // The verified token gives back a ticket. This ticket contains things like user ID, email and profile picture (all from a user's Google Account)
-        console.log(payload)
         const AuthUserId = payload.sub;
         const UserName = payload.name;
         const UserEmail = payload.email;
@@ -46,34 +45,36 @@ const router = app => {
     // POST NEW USER TO DATABASE
     app.post('/SignIn', async (request, response) => {
         let token = request.body.token
-        google_user_id = await verify(CLIENT_ID, token)
+        VerifiedTokenPayload = await verify(CLIENT_ID, token)
 
-        console.log('value of verify function: ', google_user_id)
-        if (!google_user_id) { // if value == false
+        if (!VerifiedTokenPayload) { // if value == false
             response.send('* Token verification FAIL: User not logged in *')
 
         } else {
-            response.cookie('USER_SESSION_TOKEN', token) // passing a verified token to the browser
-            //response.send('* Token verification SUCCESS: User logged in *')
-            var AuthUserId = google_user_id[0]
-            var AuthUserName = google_user_id[1]
+            // **** COOKIE IS SET HERE ****
+            console.log('setting cookie: ', token)
+            response.cookie('USER_SESSION_TOKEN', token) // passing a verified token to the browser as a cookie
+            console.log('cookie set')
+
+            var AuthUserId = VerifiedTokenPayload[0]
+            var AuthUserName = VerifiedTokenPayload[1]
             var AuthUserFirstName = AuthUserName.split(" ", 1)[0]
-            var AuthUserEmail = google_user_id[2]
-            var AuthUserImage = google_user_id[3]
+            var AuthUserEmail = VerifiedTokenPayload[2]
+            var AuthUserImage = VerifiedTokenPayload[3]
             console.log('Authenticated user details: ', AuthUserFirstName, AuthUserName, AuthUserEmail, AuthUserImage, AuthUserId)
 
             var AuthUserData = { auth_user_id: AuthUserId, email: AuthUserEmail }
             var AuthUserProfile = { first_name: AuthUserFirstName, full_name: AuthUserName, profile_picture: AuthUserImage }
-            // check if user already exists in database
 
-            try {
+
+            try { // check if user already exists in database
                 pool.query("SELECT auth_user_id FROM auth_data WHERE auth_user_id = ?", AuthUserId, function (error, result, field) {
+                    console.log('Query if user exists result: ', result)
                     if (error) throw error;
                     console.log('Query if user exists error type:', error);
-                    console.log('query if user exists result: ', result)
 
                     if (result.length === 0) {
-                        console.log('inserting new user to auth db')
+                        console.log('Inserting new user to auth db')
                         // TABLE: AUTH_DATA
                         pool.query('INSERT INTO auth_data SET ?', AuthUserData, (error, result) => {
                             if (error) throw error;
@@ -87,7 +88,7 @@ const router = app => {
 
                     } else {
                         console.log('Existing user')
-                        response.send("Existing user- signing in");
+                        // response.send("Existing user- signing in");
                     }
                 });
 
@@ -101,19 +102,22 @@ const router = app => {
     app.post('/ProtectedRoute', async (request, response) => {
         token = request.body.token
 
-        google_user_id = await verify(CLIENT_ID, token)
-        console.log('value of verify function: ', google_user_id)
-        if (!google_user_id) { //if value == false
+        VerifiedTokenPayload = await verify(CLIENT_ID, token)
+        console.log('value of verify function: ', VerifiedTokenPayload)
+        if (!VerifiedTokenPayload) { //if value == false
             response.send('* Token verification FAIL: User not logged in *')
         } else {
-            response.send('* Token verification SUCCESS: User logged in *')
+            var SuccessResponseArray = ["* Token verification SUCCESS: User logged in *", VerifiedTokenPayload[0]]
+            response.send(SuccessResponseArray)
         }
     });
 
     // Please refer to the schematic to understand how /ProtectedRoute is related to /ProtectedProfile
     app.get('/ProtectedProfile', (request, response) => {
-        console.log('in protected profile get request')
-        response.render("ProtectedProfile.ejs", { message: "Private profile message" });
+        console.log('protected profile reponse: ', request.query)
+        data = request.query.userId
+        console.log('google user id sent from front end= ', data)
+
     })
 
 
@@ -126,10 +130,9 @@ const router = app => {
     // Just type http://localhost/users to trigger this and it'll show the list of users signed up.
     app.get('/users', (request, response) => {
         pool.query(`SELECT * FROM auth_data`, (error, result) => {
+            console.log('in all users route...')
             if (error) throw error;
-
             response.send(result);
-            console.log('all users: ' + result);
         });
 
     });
@@ -145,6 +148,32 @@ const router = app => {
     app.get('/LoggedOutPage', (req, res) => {
         console.log('redirect to landing page')
         res.render('LandingPage');
+    })
+
+    // ROTUE TO DELETE A USER
+    // example: http://localhost/deleteuser?email=seanmckiernan01@gmail.com
+    // example: http://localhost/deleteuser?email=smckiernan91@gmail.com
+    app.get('/deleteuser', (request, response) => {
+        console.log(request.qurey)
+        var UserToDelete = request.query.email
+        console.log('request to delete this data:', UserToDelete);
+        pool.query(`DELETE FROM auth_data WHERE email = '${UserToDelete}'`, (error, result) => {
+            if (error) throw error;
+            console.log(response.result);
+
+            //response.redirect('/SignOut') //to delete cookie
+            pool.query(`SELECT * FROM auth_data`, (error, result) => {
+                if (error) throw error;
+                response.send(result);
+            });
+        });
+    });
+
+    // DELETE ME
+    //Protected route test
+    app.get('/UserData', (req, res) => {
+        console.log('Loading user data for user: ', req)
+        res.send('response from test')
     })
 
 
